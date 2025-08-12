45e64679f61f0124cd1954d21e041e5fdfcb1651
Vagner Oliveira <ziminny@gmail.com>
2025-08-12 13:26:39 -0700

    update basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/streamline-website">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
