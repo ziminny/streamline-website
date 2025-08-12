da42803a6c18664fcf8b78dd0a9db7ece0567cb2
Vagner Oliveira <ziminny@gmail.com>
2025-08-12 13:22:27 -0700

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/streamline-website/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
